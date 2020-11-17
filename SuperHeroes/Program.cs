c7bb1adbcbb1aca59d60a024ea85767255d6bc09
Jonathan-Lynch <jonathanlynch2001@gmail.com>
2020-11-16 18:05:59 -0600

    Added SuperHero & SuperVillian

---
+++ SuperHeroes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SuperHeroes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("What is your Super Hero name?");
+			string superName = Console.ReadLine();
+			Console.WriteLine("What is your Super Power");
+			string superPower = Console.ReadLine();
+			Console.WriteLine("SuperHero: " + superName + " | " + "Power: " + superPower);
+		}
+	}
+}
---
+++ SuperVillian/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SuperVillian
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("What is your Villian Name?");
+			string villianName = Console.ReadLine();
+			Console.WriteLine("What is your Power?");
+			string villianPower = Console.ReadLine();
+			Console.WriteLine("Who is your Nemisis?");
+			string nemisis = Console.ReadLine();
+
+			Console.WriteLine("Name: {0} | Power: {1} | Nemisis: {2}", villianName, villianPower, nemisis);
+		}
+	}
+}
